9c5c4660b73603b5aba8e9667509aba3ed442241
vercetti322 <vercettitommy322@gmail.com>
2024-08-03 10:30:08 +0530

    project init

---
+++ src/main/java/com/vercetti/ecom_app/EcomAppApplication.java
@@ -0,0 +1,11 @@
+package com.vercetti.ecom_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcomAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vercetti/ecom_app/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vercetti.ecom_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
