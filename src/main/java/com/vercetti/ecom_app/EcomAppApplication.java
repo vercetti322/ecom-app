d66047e4ac762cedcb4563bd6d2915468ef81033
vercetti322 <vercettitommy322@gmail.com>
2024-08-03 12:00:49 +0530

    db config done!

--- src/main/java/com/vercetti/ecom_app/EcomAppApplication.java
+++ src/main/java/com/vercetti/ecom_app/EcomAppApplication.java
@@ -1,6 +1,5 @@
 package com.vercetti.ecom_app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
